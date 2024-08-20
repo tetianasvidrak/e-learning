28b276868e028b605f95ff2fe65a048888427867
tetianasvidrak <svidrak.tetiana@gmail.com>
2024-08-20 18:25:58 +0200

    added background video to login page

--- src/pages/sign-in/sign-in.jsx
+++ src/pages/sign-in/sign-in.jsx
@@ -1,15 +1,19 @@
+import { useSelector } from "react-redux";
+
 import Loader from "../../components/loader";
 import SignInForm from "../../components/sign-in-form";
-// import Loader from "../../components/loader";
-import { useSelector } from "react-redux";
+import VideoBackground from "../../components/video-bg";
+
+import video from "../../assets/videos/bg-video-1.mp4";
 
 const SignIn = () => {
   const { loading } = useSelector((state) => state.auth);
   console.log(loading);
   return (
     <main className="signin-page">
-      <SignInForm />
       {loading === "pending" && <Loader />}
+      <SignInForm />
+      <VideoBackground video={video} />
     </main>
   );
 };
